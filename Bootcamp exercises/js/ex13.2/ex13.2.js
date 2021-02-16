22b195aa47151ae0eea069e059ec6b10c98f5129
maorte <maor884@gmail.com>
2021-02-16 13:03:02 +0200

    more exercises

---
+++ Bootcamp exercises/js/ex13.2/ex13.2.js
@@ -0,0 +1,17 @@
+const populations = [1440, 2440, 3440, 4440];
+
+const percentageOfWorld2 = (population) =>
+	((population / 7900) * 100).toFixed(1);
+
+function populationPercentages(populations) {
+	const percentages = [];
+	let i = 0;
+	while (i < populations.length) {
+		const element = percentageOfWorld2(populations[i]);
+		percentages.push(element);
+		i++;
+	}
+	console.log(percentages);
+}
+
+console.log(populationPercentages(populations));
