8c2a7ab2e2183a8b3b200c6a9e00105bce5d1af9
maorte <maor884@gmail.com>
2021-02-16 14:55:54 +0200

    more exercises

--- Bootcamp exercises/js/ex13.4/ex13.4.js
+++ Bootcamp exercises/js/ex13.4/ex13.4.js
@@ -1,4 +1,4 @@
-function step(N) {
+function ninjaStep(N) {
 	let mat = [];
 	for (let i = 0; i < N; i++) {
 		mat[i] = [];
@@ -8,4 +8,10 @@ function step(N) {
 		console.log(`'${mat[i].join('')}'`);
 	}
 }
-step(3);
+
+function regularStep(N) {
+	for (let i = 1; i <= N; i++) {
+		console.log(`'${'#'.repeat(i)}${' '.repeat(N - i)}'`);
+	}
+}
+ninjaStep(3);
