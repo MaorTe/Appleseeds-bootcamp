eddd29d2c53c07bfd6c4d668c1f64bffeb3f68dd
maorte <maor884@gmail.com>
2021-02-18 11:41:56 +0200

    weekend assignment

--- Weekend assignments/JSweekendexercises/ex2.3.js
+++ Weekend assignments/JSweekendexercises/ex2.3.js
@@ -1,1 +1,11 @@
-console.log(typeof 1);
+function findNextSquare(sq) {
+	let root;
+	// let nextroot;
+	if (Math.sqrt(sq) % 1 === 0) {
+		root = Math.sqrt(sq);
+		return Math.pow(root + 1, 2);
+	}
+	return -1;
+}
+
+console.log(findNextSquare(625));
