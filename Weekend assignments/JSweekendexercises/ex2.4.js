a978f733bd53e8d20881be6f99bae91d85742249
maorte <maor884@gmail.com>
2021-02-18 12:36:03 +0200

    weekend assignment

--- Weekend assignments/JSweekendexercises/ex2.4.js
+++ Weekend assignments/JSweekendexercises/ex2.4.js
@@ -1,2 +1,11 @@
-findUniq([1, 1, 1, 2, 1, 1]) === 2;
-findUniq([0, 0, 0.55, 0, 0]) === 0.55;
+function findUniq(arr) {
+	for (let i = 0; i < arr.length - 1; i++) {
+		if (arr[i] !== arr[i + 1]) {
+			return arr[i + 1];
+		}
+	}
+}
+
+arr = [1, 1, 1, 2, 1, 1];
+console.log(findUniq(arr));
+// findUniq([0, 0, 0.55, 0, 0]) === 0.55;
