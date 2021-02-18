f2c164ed2a061f243d6bf8a72253173b66df8cee
maorte <maor884@gmail.com>
2021-02-18 15:04:41 +0200

    weekend assignment

---
+++ Weekend assignments/JSweekendexercises/ex3.2.js
@@ -0,0 +1,14 @@
+let arr = [
+	[2, 0],
+	[5, 4],
+	[9, 5],
+	[3, 6],
+];
+// let sum1 = 0;
+// const sum = arr.forEach((el) => {
+// 	return (sum1 += el[0] - el[1]);
+// });
+let sum = arr.reduce((acc, next) => {
+	return acc >= 0 ? (acc += next[0] - next[1]) : 'invalid input,less than 0';
+}, 0);
+console.log(sum);
