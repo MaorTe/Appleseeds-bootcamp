952ed962820880b3840e3541c866e0c0ad1e4c8e
maorte <maor884@gmail.com>
2021-02-18 15:50:59 +0200

    weekend assignment

--- Weekend assignments/JSweekendexercises/ex4.1.js
+++ Weekend assignments/JSweekendexercises/ex4.1.js
@@ -1,6 +1,19 @@
 function fib(n) {
+	let sum = 0;
 	if (n <= 1) return n;
-	return fib(n - 1) + fib(n - 2);
+
+	return (sum = fib(n - 1) + fib(n - 2));
 }
 
-console.log(fib(12));
+function generateFib(n) {
+	let fib = [];
+	fib[0] = 0;
+	// console.log((fib[0] = 0));
+	fib[1] = 1;
+	// console.log((fib[1] = 1));
+	for (let i = 2; i <= n; i++) {
+		fib[i] = fib[i - 2] + fib[i - 1];
+	}
+	console.log(fib.flat(2));
+}
+generateFib(10);
