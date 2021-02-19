56ad728aa70663c2bb1e64e4e91d0439062416bb
maorte <maor884@gmail.com>
2021-02-19 18:02:00 +0200

    more weekend exercises

---
+++ Weekend assignments/JSweekendexercises/ex5.4.js
@@ -0,0 +1,9 @@
+function toWeirdCase(str) {
+	arr = str.trim().split('');
+	for (let i = 0; i < arr.length; i += 2) {
+		if (arr[i] === ' ') i++;
+		arr[i] = arr[i].toUpperCase();
+	}
+	return arr.join('');
+}
+console.log(toWeirdCase('Weird string case cas'));
