3ba46860017f03256c28c03fc9639c92df7983ae
maorte <maor884@gmail.com>
2021-02-21 01:46:32 +0200

    final fixes

--- Weekend assignments/JSweekendexercises/ex6.4.js
+++ Weekend assignments/JSweekendexercises/ex6.4.js
@@ -3,15 +3,29 @@ function uniqueLetters(str) {
 		return 'string must be letters from a..z and A..Z only.';
 	else if (str === '') str = ' ';
 
-	var seen = {};
+	let seen = {};
 	let arr = str.toLowerCase().split('');
-	arr.filter((item) => (seen[item] = seen[item] + 1 || 1));
-	for (const value of Object.values(seen)) {
-		return value <= 1;
-	}
+	arr.forEach((item) => (seen[item] = seen[item] + 1 || 1));
+	return !(Object.values(seen).find((value) => value > 1) > 1);
 }
 
+console.log(uniqueLetters(''));
 console.log(uniqueLetters('Dermatoglyphics'));
 console.log(uniqueLetters('aba'));
-console.log(uniqueLetters(''));
 console.log(uniqueLetters('moOse'));
+
+//Ex6.4 - isogram
+// const isogram = (str) => {
+// 	str = str.toLowerCase();
+// 	const obj = { arr: [] };
+// 	str
+// 		.split('')
+// 		.forEach((letter) =>
+// 			obj.hasOwnProperty(letter) ? obj.arr.push(false) : (obj[letter] = 1)
+// 		);
+// 	return obj.arr.every(Boolean);
+// };
+
+// console.log(isogram('Dermatoglyphics'));
+// console.log(isogram('aba'));
+// console.log(isogram('moOse'));
