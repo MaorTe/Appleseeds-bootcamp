8db736180991957b0d720422547f7b0c8868140a
maorte <maor884@gmail.com>
2021-02-21 00:47:07 +0200

    fixes

--- Weekend assignments/JSweekendexercises/ex5.8.js
+++ Weekend assignments/JSweekendexercises/ex5.8.js
@@ -1,10 +1,8 @@
-const LongestWord = (str) => {
-	debugger;
+const longestWord = (str) => {
 	return str
 		.trim()
 		.split(' ')
 		.reduce((acc, cur) => (acc.length >= cur.length ? acc : cur));
 };
 
-// console.log(LongestWord(`    I'm the  longest   word or are you?  `));
-console.log(LongestWord(`the aaa bbb ccc ddd eee`));
+console.log(longestWord(`    I'm the  longest   word or are you?  `));
