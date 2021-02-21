03168536993a10d55133e24ce41224e237be2e6d
maorte <maor884@gmail.com>
2021-02-21 02:12:57 +0200

    final fix

--- Weekend assignments/JSweekendexercises/ex6.4.js
+++ Weekend assignments/JSweekendexercises/ex6.4.js
@@ -13,19 +13,3 @@ console.log(uniqueLetters(''));
 console.log(uniqueLetters('Dermatoglyphics'));
 console.log(uniqueLetters('aba'));
 console.log(uniqueLetters('moOse'));
-
-//Ex6.4 - isogram
-// const isogram = (str) => {
-// 	str = str.toLowerCase();
-// 	const obj = { arr: [] };
-// 	str
-// 		.split('')
-// 		.forEach((letter) =>
-// 			obj.hasOwnProperty(letter) ? obj.arr.push(false) : (obj[letter] = 1)
-// 		);
-// 	return obj.arr.every(Boolean);
-// };
-
-// console.log(isogram('Dermatoglyphics'));
-// console.log(isogram('aba'));
-// console.log(isogram('moOse'));
