113f0d35c09a1dbc2b99a51a0dfd5c3f11173267
maorte <maor884@gmail.com>
2021-02-23 21:34:51 +0200

    more exercises

---
+++ Bootcamp exercises/js/ex21.1/script.js
@@ -0,0 +1,15 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+button.addEventListener('click', function () {
+	if (document.querySelector('p')) {
+		document.querySelector('p').remove();
+	}
+	const message = document.createElement('p');
+	if (input.value > 18) {
+		message.textContent = 'you can drink a beer';
+		document.body.appendChild(message);
+	} else {
+		message.textContent = 'you’re too young';
+		document.body.appendChild(message);
+	}
+});
