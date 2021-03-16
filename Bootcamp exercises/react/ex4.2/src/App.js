2b27c16ed5af499dbbbedfb9e36977c4531b2f52
maorte <maor884@gmail.com>
2021-03-16 14:20:48 +0200

    more exercises

--- Bootcamp exercises/react/ex4.2/src/App.js
+++ Bootcamp exercises/react/ex4.2/src/App.js
@@ -2,26 +2,28 @@ import React from 'react';
 import './App.css';
 import Container from './components/Container';
 
-function App() {
-	return (
-		<div>
-			<Container
-				title="title1"
-				desc="desc1"
-				link1="SHARE"
-				link2="EXPLORE"></Container>
-			<Container
-				title="title2"
-				desc="desc2"
-				link1="SHARE"
-				link2="EXPLORE"></Container>
-			<Container
-				title="title3"
-				desc="desc3"
-				link1="SHARE"
-				link2="EXPLORE"></Container>
-		</div>
-	);
+class App extends React.Component {
+	render() {
+		return (
+			<div style={{ display: 'flex' }}>
+				<Container
+					title="title1"
+					desc="desc1"
+					link1="SHARE"
+					link2="EXPLORE"></Container>
+				<Container
+					title="title2"
+					desc="desc2"
+					link1="SHARE"
+					link2="EXPLORE"></Container>
+				<Container
+					title="title3"
+					desc="desc3"
+					link1="SHARE"
+					link2="EXPLORE"></Container>
+			</div>
+		);
+	}
 }
 
 export default App;
