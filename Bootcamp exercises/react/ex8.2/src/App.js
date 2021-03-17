66cd48ccdaf407ed1fbd3d7198fe6cc160ea2531
maorte <maor884@gmail.com>
2021-03-17 05:08:40 +0200

    more exercises

---
+++ Bootcamp exercises/react/ex8.2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Box from './components/Box';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<Box class="box1" />
+				<Box class="box2" />
+				<Box class="box3" />
+			</div>
+		);
+	}
+}
+export default App;
