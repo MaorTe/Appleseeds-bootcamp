c69442ad4779b2a77d679e2d1f0df9b23b782036
maorte <maor884@gmail.com>
2021-03-20 21:34:39 +0200

    single player done

--- Weekend assignments/react-dice/src/App.js
+++ Weekend assignments/react-dice/src/App.js
@@ -1,69 +1,123 @@
 import React from 'react';
 import './App.css';
-
+import PlayerPanel from './components/PlayerPanel';
+import Button from './components/Button';
+import DiceImage from './components/DiceImage';
+import dice1 from './img/dice-1.png';
+import dice2 from './img/dice-2.png';
+import dice3 from './img/dice-3.png';
+import dice4 from './img/dice-4.png';
+import dice5 from './img/dice-5.png';
+import dice6 from './img/dice-6.png';
+import ScoreInput from './components/ScoreInput';
 class App extends React.Component {
-	state = { aa: true };
+	state = {
+		GameStarted: false,
+		scoreP1: 0,
+		scoreP2: 0,
+		currentScoreP1: 0,
+		currentScoreP2: 0,
+		randNum1: 1,
+		randNum2: 1,
+		images: [dice1, dice2, dice3, dice4, dice5, dice6],
+		scoreLimit: 100,
+	};
 
-	render() {
+	componentDidMount() {
+		//save initial state for new game
+		this.initialState = this.state;
+	}
+	renderNewGame = () => {
 		return (
 			<div className="wrapper clearfix">
-				<div className="player-0-panel active">
-					<div className="player-name" id="name-0">
-						Player 1
-					</div>
-					<div className="player-score" id="score-0">
-						0
-					</div>
-					<div className="player-current-box">
-						<div className="player-current-label">Current</div>
-						<div className="player-current-score" id="current-0">
-							0
-						</div>
-					</div>
-				</div>
+				<PlayerPanel
+					classes="player-0-panel active"
+					nameId="name-0"
+					scoreId="score-0"
+					currentId="current-0"
+					playerName="Player 1"
+					score={this.state.scoreP1}
+					currentScore={this.state.currentScoreP1}></PlayerPanel>
+				<PlayerPanel
+					classes="player-1-panel"
+					nameId="name-1"
+					scoreId="score-1"
+					currentId="current-1"
+					playerName="Player 2"
+					score={this.state.scoreP2}
+					currentScore={this.state.currentScoreP2}></PlayerPanel>
 
-				<div className="player-1-panel">
-					<div className="player-name" id="name-1">
-						Player 2
-					</div>
-					<div className="player-score" id="score-1">
-						0
-					</div>
-					<div className="player-current-box">
-						<div className="player-current-label">Current</div>
-						<div className="player-current-score" id="current-1">
-							0
-						</div>
-					</div>
-				</div>
-
-				<button className="btn-new">
-					<i className="ion-ios-plus-outline"></i>New game
-				</button>
-				<button className="btn-roll">
-					<i className="ion-ios-loop"></i>Roll dice
-				</button>
-				<button className="btn-hold">
-					<i className="ion-ios-download-outline"></i>Hold
-				</button>
-
-				<img src="./img/dice-1.png" alt="Dice" className="dice hidden" />
-				<img src="./img/dice-1.png" alt="Dice" className="dice dice-2 hidden" />
-
-				<div className="input-wrapper">
-					<span className="input-wrapper-desc">Score to collect:</span>
-					<input
-						type="text"
-						className="score-input"
-						id="finalScoreInput"
-						value="100"
-					/>
-				</div>
-				<a href="#" className="help">
-					<i className="ion-help-circled"></i>
-				</a>
+				<Button
+					classes="btn-new"
+					icon="ion-ios-plus-outline"
+					btnName="New game"
+					onClick={() => {
+						// let prevScore = this.state.scoreLimit;
+						// console.log(prevScore);
+						this.setState(this.initialState);
+						// this.setState({
+						// 	scoreLimit: prevScore,
+						// });
+					}}></Button>
+				{!this.isPlayerWon() && (
+					<Button
+						classes="btn-roll"
+						icon="ion-ios-loop"
+						btnName="Roll dice"
+						onClick={this.randomNumber}></Button>
+				)}
+				{!this.isPlayerWon() && (
+					<Button
+						classes="btn-hold"
+						icon="ion-ios-download-outline"
+						btnName="Hold"
+						name="scoreLimit"
+						onClick={this.setScoreAndMoveToPlayer}></Button>
+				)}
+				{this.state.GameStarted && (
+					<DiceImage
+						classes="dice"
+						diceNum={this.state.images[this.state.randNum1 - 1]}></DiceImage>
+				)}
+				{this.state.GameStarted && (
+					<DiceImage
+						classes="dice dice-2"
+						diceNum={this.state.images[this.state.randNum2 - 1]}></DiceImage>
+				)}
+				<ScoreInput
+					name="scoreLimit"
+					value={this.state.scoreLimit}
+					onChange={this.handleChange}
+					disabled={this.state.GameStarted}></ScoreInput>
 			</div>
 		);
+	};
+	setScoreAndMoveToPlayer = () => {
+		this.setState({ scoreP1: this.state.currentScoreP1 });
+	};
+	handleChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+	isPlayerWon = () => {
+		if (this.state.currentScoreP1 > this.state.scoreLimit) {
+			console.log('player won');
+			return true;
+		}
+	};
+	randomNumber = () => {
+		//random number between 1 and 6
+		const randNum1 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+		const randNum2 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+		this.setState({
+			currentScoreP1: this.state.currentScoreP1 + randNum1 + randNum2,
+			randNum1,
+			randNum2,
+			GameStarted: true,
+		});
+	};
+	render() {
+		// return !this.isPlayerWon() ? this.renderNewGame() : null;
+		return this.renderNewGame();
 	}
 }
 
