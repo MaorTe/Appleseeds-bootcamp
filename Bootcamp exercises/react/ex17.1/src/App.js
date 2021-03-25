9269bcffbfb50b272dd833d229ccd88a4c7d19d2
maorte <maor884@gmail.com>
2021-03-25 21:40:30 +0200

    more exercises

--- Bootcamp exercises/react/ex17.1/src/App.js
+++ Bootcamp exercises/react/ex17.1/src/App.js
@@ -1,7 +1,19 @@
+import React from 'react';
 import './App.css';
-
-function App() {
-	return <div className="container"></div>;
+import api from './api/api.js';
+class App extends React.Component {
+	state = { users: [] };
+	async componentDidMount() {
+		const response = await api.get(`/users`);
+		this.setState({ users: response });
+		setTimeout(() => {
+			console.log(this.state.users);
+		}, 300);
+	}
+	render() {
+		console.log(this.state.users);
+		return <div></div>;
+	}
 }
 
 export default App;
---
+++ Bootcamp exercises/react/ex17.1/src/api/api.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+	baseURL: 'http://605b2c7927f0050017c0666b.mockapi.io',
+});
