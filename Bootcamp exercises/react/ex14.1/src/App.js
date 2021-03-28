e584012d26ab7388e4be8be4fe3f75aa45b3a0e5
maorte <maor884@gmail.com>
2021-03-28 16:03:24 -0600

    more exercises

--- Bootcamp exercises/react/ex14.1/src/App.js
+++ Bootcamp exercises/react/ex14.1/src/App.js
@@ -1,7 +1,20 @@
+import React from 'react';
 import './App.css';
-
-function App() {
-	return <div className="container"></div>;
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.inputRef = React.createRef();
+	}
+	componentDidMount() {
+		this.inputRef.current.focus();
+	}
+	render() {
+		return (
+			<div>
+				<input type="text" ref={this.inputRef} defaultValue="aaa" />
+			</div>
+		);
+	}
 }
 
 export default App;
