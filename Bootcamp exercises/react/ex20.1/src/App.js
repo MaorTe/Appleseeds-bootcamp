b316ab1d5ceb9972084be3988c76be12543d129f
maorte <maor884@gmail.com>
2021-03-31 21:30:30 +0300

    more exercises

--- Bootcamp exercises/react/ex20.1/src/App.js
+++ Bootcamp exercises/react/ex20.1/src/App.js
@@ -1,46 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import API from './api/API';
 import './App.css';
 import DataContainer from './components/DataContainer';
 
 const App = () => {
-	const [toggle, setToggle] = useState('Show');
-	const [results, setResults] = useState([]);
+	const [toggle, setToggle] = useState(false);
 
-	// useEffect(() => {
-	// 	const fetch = async () => {
-	// 		const { data } = await API.get('1/');
-	// 		setResults([...results, data.title, data.director]);
-	// 	};
-	// 	if (results.length < 1) fetch();
-	// }, []);
-
-	useEffect(() => {
-		setTimeout(() => {
-			const toggleFetch = async () => {
-				const { data } = await API.get('1/');
-				console.log(data);
-				setResults([...results, data.title, data.director, data.characters]);
-			};
-			// if (toggle)
-
-			toggleFetch();
-		}, 3000);
-	}, [toggle]);
-	const renderResults = results.map((result, i) => (
-		<div key={i}>
-			<div>{result}</div>
-		</div>
-	));
-	const handleToggle = () => {
-		return toggle === 'Show' ? setToggle('Hide') : setToggle('Show');
-	};
 	return (
 		<div className="container">
-			<button onClick={handleToggle}>{toggle}</button>
-			{toggle === 'Hide' && (
-				<DataContainer data={renderResults}></DataContainer>
-			)}
+			<button onClick={() => setToggle((toggle) => !toggle)}>
+				{toggle ? 'Hide' : 'Show'}
+			</button>
+			{toggle && <DataContainer />}
 		</div>
 	);
 };
--- Bootcamp exercises/react/ex20.1/src/components/DataContainer.js
+++ Bootcamp exercises/react/ex20.1/src/components/DataContainer.js
@@ -1,4 +1,34 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import API from '../api/API';
+
 const DataContainer = (props) => {
-	return <div className="container">{props.data}</div>;
+	const [results, setResults] = useState([]);
+
+	useEffect(() => {
+		const CancelToken = axios.CancelToken;
+		const source = CancelToken.source();
+		const toggleFetch = async () => {
+			try {
+				const { data } = await API.get('1/', {
+					cancelToken: source.token,
+				});
+				setResults([data]);
+			} catch (e) {
+				if (axios.isCancel(e)) {
+					console.log(e.message);
+				} else {
+					console.log(e.message);
+				}
+			}
+		};
+		toggleFetch();
+
+		return () => {
+			source.cancel('cancelled by user');
+		};
+	}, []);
+
+	return <div className="container">{results && JSON.stringify(results)}</div>;
 };
 export default DataContainer;
