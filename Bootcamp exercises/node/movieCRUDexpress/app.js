094baa0a592a8318c591a75a8f449709858fa0ab
maorte <maor884@gmail.com>
2021-04-15 05:54:02 +0300

    more exercises

---
+++ Bootcamp exercises/node/movieCRUDexpress/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const { findMovies, add, findMovie, remove, update } = require('./utils/utils');
+
+app.use(express.json());
+
+//get all movies
+app.get('/api/movies', (req, res) => {
+	const movies = findMovies();
+	res.status(200).send(movies);
+});
+
+//Add a movie
+app.post('/api/movies', (req, res) => {
+	const newMovie = add(req.body);
+	res.status(200).send(newMovie);
+});
+
+//get a single movie
+app.get('/api/movies/:id', (req, res) => {
+	const { id } = req.params;
+	const movie = findMovie(id);
+	res.status(200).send(movie);
+});
+
+//update an existing movie
+app.put('/api/movies/:id', (req, res) => {
+	const { id } = req.params;
+	const movieToUpdate = update(id, 'aaa');
+	res.status(200).send(movieToUpdate);
+});
+
+//deletes an existing movie
+app.delete('/api/movies/:id', (req, res) => {
+	const { id } = req.params;
+	const movieToRemove = remove(id);
+	res.status(200).send(movieToRemove);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+	console.log('listening...');
+});
