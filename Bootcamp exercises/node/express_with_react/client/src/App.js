e259aad5ca5635a0946b4d1a5db82c88f5831218
maorte <maor884@gmail.com>
2021-04-18 22:11:47 +0300

    some change

--- Bootcamp exercises/node/express_with_react/client/src/App.js
+++ Bootcamp exercises/node/express_with_react/client/src/App.js
@@ -4,15 +4,20 @@ import './App.css';
 //app
 const App = () => {
 	const [value, setValue] = useState('');
+	const [userData, setUserData] = useState([]);
+	// const [fetched, setFetched] = useState(false);
 	useEffect(() => {
 		const fetchData = async () => {
-			const data = await api.get('test');
+			const { data } = await api.get('test');
+			setUserData(data);
 			console.log(data);
+			// setUserData(JSON.stringify(data));
 		};
 		fetchData();
 	}, []);
 	const handleSubmit = async () => {
 		await api.post('form', { formValues: value });
+		// setFetched(true);
 	};
 	return (
 		<div className="App">
@@ -22,6 +27,16 @@ const App = () => {
 				onChange={(e) => setValue(e.target.value)}
 			/>
 			<button onClick={handleSubmit}>Submit</button>
+			<p>
+				{userData.map((user, i) => (
+					<div key={i} style={{ border: '1px solid black', margin: '10px' }}>
+						<span style={{ display: 'block' }}>{user.id},</span>
+						<span style={{ display: 'block' }}>{user.cash},</span>
+						<span style={{ display: 'block' }}>{user.credit},</span>
+						<span style={{ display: 'block' }}>{user.isActive.toString()}</span>
+					</div>
+				))}
+			</p>
 		</div>
 	);
 };
