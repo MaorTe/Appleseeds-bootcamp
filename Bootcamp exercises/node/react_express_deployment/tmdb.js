9f9ed65f152af72cacff8d55f06c5e9970224380
maorte <maor884@gmail.com>
2021-04-19 16:47:33 +0300

    small fix

--- Bootcamp exercises/node/react_express_deployment/tmdb.js
+++ Bootcamp exercises/node/react_express_deployment/tmdb.js
@@ -1,11 +1,11 @@
-const axios = require('axios');
+const axios = require('axios').default;
+// import axios from 'axios';
 const { tmdb } = require('./config/keys');
 
 const popularMovies = async () => {
-	// @ts-ignore
 	const data = await axios.get(
 		`https://api.themoviedb.org/3/movie/popular?api_key=${tmdb}&language=en-US&page=1`
 	);
 	return data;
 };
-module.exports = { popularMovies };
+module.exports = popularMovies;
