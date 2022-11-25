efd3f83187fa8c6c4c244c240d0d5fc0beb5b381
maorte <maor884@gmail.com>
2022-11-25 23:08:20 +0200

    some change

--- Bootcamp exercises/js/ex2.2/ex2.2.js
+++ Bootcamp exercises/js/ex2.2/ex2.2.js
@@ -1,6 +1,5 @@
 function tellFortune(jobTitle, location, partnerName, numberOfChildren) {
-	return `You will be a ${jobTitle} in ${location} and married to
+   return `You will be a ${jobTitle} in ${location} and married to
 ${partnerName} with ${numberOfChildren} children.`;
 }
-
-console.log(tellFortune('programmer', 'Tel Aviv', 'Sharon', '2'));
+console.log(tellFortune('Programmer', 'Tel Aviv', 'Sharon', '2'));
